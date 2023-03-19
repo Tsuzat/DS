e90dba7f201361fc8b6a155f043fbb53c7fbc6a1
tsuzat <aloksingh772002@gmail.com>
2023-03-19 12:37:13 +0530

    add main.dart

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:dlds/screens/home_page.dart';
+import 'package:dlds/theme.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
+import 'package:system_theme/system_theme.dart';
+import 'package:window_manager/window_manager.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemTheme.accentColor.load();
+
+  await flutter_acrylic.Window.initialize();
+  await flutter_acrylic.Window.hideWindowControls();
+
+  await WindowManager.instance.ensureInitialized();
+
+  windowManager.waitUntilReadyToShow().then((_) async {
+    await windowManager.setTitleBarStyle(
+      TitleBarStyle.hidden,
+      windowButtonVisibility: false,
+    );
+    await windowManager.setSize(const Size(900, 700));
+    await windowManager.setMinimumSize(const Size(900, 700));
+    await windowManager.center();
+    await windowManager.show();
+    await windowManager.setPreventClose(true);
+    await windowManager.setSkipTaskbar(false);
+  });
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  // private navigators
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => AppTheme(),
+      builder: (context, _) {
+        final appTheme = context.watch<AppTheme>();
+        return FluentApp(
+          title: "DLDS",
+          themeMode: appTheme.mode,
+          debugShowCheckedModeBanner: false,
+          color: appTheme.color,
+          darkTheme: FluentThemeData(
+            brightness: Brightness.dark,
+            accentColor: appTheme.color,
+            visualDensity: VisualDensity.standard,
+            focusTheme: FocusThemeData(
+              glowFactor: is10footScreen() ? 2.0 : 0.0,
+            ),
+          ),
+          theme: FluentThemeData(
+            accentColor: appTheme.color,
+            visualDensity: VisualDensity.standard,
+            focusTheme: FocusThemeData(
+              glowFactor: is10footScreen() ? 2.0 : 0.0,
+            ),
+          ),
+          builder: (context, child) {
+            return Directionality(
+              textDirection: TextDirection.ltr,
+              child: NavigationPaneTheme(
+                data: NavigationPaneThemeData(
+                  backgroundColor: appTheme.windowEffect !=
+                          flutter_acrylic.WindowEffect.disabled
+                      ? Colors.transparent
+                      : null,
+                ),
+                child: child!,
+              ),
+            );
+          },
+          routes: {"/": (context) => const HomePage()},
+          initialRoute: "/",
+        );
+      },
+    );
+  }
+}
