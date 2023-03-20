73d7b980cbeba731e02bc4053965304ffb66b4a7
tsuzat <aloksingh772002@gmail.com>
2023-03-20 10:26:17 +0530

    fix: windows destroys on clicking close

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -25,6 +25,11 @@ class _HomePageState extends State<HomePage> with WindowListener {
   }
 
   @override
+  void onWindowClose() {
+    windowManager.destroy();
+  }
+
+  @override
   Widget build(BuildContext context) {
     AppTheme appTheme = context.watch<AppTheme>();
 
@@ -63,47 +68,6 @@ class _HomePageState extends State<HomePage> with WindowListener {
     );
   }
 }
-
-// class WindowButtons extends StatefulWidget {
-//   const WindowButtons({Key? key}) : super(key: key);
-
-//   @override
-//   _WindowButtonsState createState() => _WindowButtonsState();
-// }
-
-// class _WindowButtonsState extends State<WindowButtons> {
-//   void maximizeOrRestore() {
-//     setState(() {
-//       appWindow.maximizeOrRestore();
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       children: [
-//         MinimizeWindowButton(colors: buttonColors),
-//         appWindow.isMaximized
-//             ? RestoreWindowButton(
-//                 colors: buttonColors,
-//                 onPressed: maximizeOrRestore,
-//               )
-//             : MaximizeWindowButton(
-//                 colors: buttonColors,
-//                 onPressed: maximizeOrRestore,
-//               ),
-//         CloseWindowButton(colors: closeButtonColors),
-//       ],
-//     );
-//   }
-// }
-
-// class WindowButtons extends StatefulWidget {
-//   const WindowButtons({super.key});
-
-//   @override
-//   State<WindowButtons> createState() => _WindowButtonsState();
-// }
 
 class WindowButtons extends StatelessWidget {
   const WindowButtons({Key? key}) : super(key: key);
