8c1e27fa6f0097831a2f4a2c963fedff1b481999
tsuzat <aloksingh772002@gmail.com>
2023-03-22 23:20:05 +0530

    migrated to `bitsdojo_window`

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:dlds/screens/home_page.dart';
 import 'package:dlds/theme.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
 import 'package:system_theme/system_theme.dart';
-import 'package:window_manager/window_manager.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
@@ -14,19 +14,14 @@ void main() async {
   await flutter_acrylic.Window.initialize();
   await flutter_acrylic.Window.hideWindowControls();
 
-  await WindowManager.instance.ensureInitialized();
-
-  windowManager.waitUntilReadyToShow().then((_) async {
-    await windowManager.setTitleBarStyle(
-      TitleBarStyle.hidden,
-      windowButtonVisibility: false,
-    );
-    await windowManager.setSize(const Size(900, 700));
-    await windowManager.setMinimumSize(const Size(900, 700));
-    await windowManager.center();
-    await windowManager.show();
-    await windowManager.setPreventClose(true);
-    await windowManager.setSkipTaskbar(false);
+  doWhenWindowReady(() {
+    final win = appWindow;
+    const initialSize = Size(1000, 600);
+    win.minSize = initialSize;
+    win.size = initialSize;
+    win.alignment = Alignment.center;
+    win.title = "DLDS";
+    win.show();
   });
 
   runApp(const MyApp());
