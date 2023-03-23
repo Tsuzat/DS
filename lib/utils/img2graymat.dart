cdb7c7d0f292b6d1f035b08eee9d3896c2aa3434
tsuzat <aloksingh772002@gmail.com>
2023-03-23 14:31:39 +0530

    convert an image to grayscale matrix

--- lib/utils/img2graymat.dart
+++ lib/utils/img2graymat.dart
@@ -1,19 +1,19 @@
 import 'dart:io';
 import 'package:image/image.dart';
 
-List<List<int>> img2mat(File imageFile) {
+List<List<double>> img2mat(File imageFile) {
   var image = decodeImage(imageFile.readAsBytesSync());
 
   var gimg = grayscale(image!);
 
   // Convert image to grayscale matrix
-  List<List<int>> grayscaleMatrix = [];
+  List<List<double>> grayscaleMatrix = [];
   for (int y = 0; y < gimg.height; y++) {
-    List<int> row = [];
+    List<double> row = [];
     for (int x = 0; x < gimg.width; x++) {
       Pixel pixel = gimg.getPixel(x, y);
-      int grayValue =
-          ((pixel.r * 0.299) + (pixel.g * 0.587) + (pixel.b * 0.114)).round();
+      double grayValue =
+          ((pixel.r * 0.299) + (pixel.g * 0.587) + (pixel.b * 0.114));
       row.add(grayValue);
     }
     grayscaleMatrix.add(row);
