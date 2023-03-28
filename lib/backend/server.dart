deb3a3f9ae37920e531b9f626ad93ff3e3b3e86c
tsuzat <aloksingh772002@gmail.com>
2023-03-28 12:17:34 +0530

    bind frontend to backend

---
+++ lib/backend/server.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+
+final dio = Dio();
+
+/// Checks if the backend is running on http://127.0.0.1:8000/
+
+Future<bool> checkBackend() async {
+  try {
+    Response response = await dio.get("http://127.0.0.1:8080");
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (e) {
+    return false;
+  }
+}
+
+Future<Map<String, dynamic>> svdBackEnd(Map<String, dynamic> data) async {
+  Response response = await dio.post("http://127.0.0.1:8000/svd", data: data);
+  Map<String, dynamic> respData = response.data;
+  return respData;
+}
