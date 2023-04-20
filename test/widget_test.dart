b3a807180671ddd26f8cb80899e09a09b2ff8387
tsuzat <aloksingh772002@gmail.com>
2023-04-20 12:50:02 +0530

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dlds/main.dart';
+import 'package:ds/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
